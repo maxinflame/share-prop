f0e1446d7ca2aeb40db534ef097c565b7ed9c7e1
Maksim Smirnov <maxinflame@yandex.ru>
2023-04-08 23:25:41 +0300

    category add form

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const categories = [];
+
 const financeShares = [
   {
     name: 'СБЕР',
@@ -35,3 +37,39 @@ oilShares.forEach(item => {
   const share = new Share(item.name, item.ticker, item.lot);
   oil.addShare(share)
 })
+
+const createCategoryAddForm = () => {
+  const openButton = document.querySelector('.add-category-button');
+  const closeButton = document.querySelector('.category-add-popup__close');
+  const popup = document.querySelector('.category-add-popup');
+  const form = document.querySelector('.category-add-form');
+  const input = document.querySelector('.category-add-input');
+
+  const showPopup = () => {
+    popup.classList.add('active');
+  }
+
+  const hidePopup = () => {
+    popup.classList.remove('active');
+    input.value = '';
+  }
+
+  const createCategory = () => {
+    const category = new Category(input.value);
+    categories.push(category);
+  }
+
+  openButton.addEventListener('click', showPopup)
+  closeButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    hidePopup()
+  })
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    createCategory();
+    hidePopup();
+  });
+}
+
+createCategoryAddForm()
